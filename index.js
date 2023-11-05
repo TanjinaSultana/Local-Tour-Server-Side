fec055972e1c2becf901fce354be2998d0109f59
Tanjina sultana <tanjinasultana388@gmail.com>
2023-11-06 03:26:08 +0600

    data are send in database and get in client side and also data update and delete are done

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port =process.env.PORT || 5000;
 
 
+app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.asca0m7.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,19 +23,68 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
+    const database = client.db("productDB");
+    const serviceCollection = database.collection("product");
+    app.get('/service', async(req,res) =>{
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+app.post('/service',async(req,res) =>{
+    const newProduct = req.body;
+    console.log(newProduct);
+   
+    const result = await serviceCollection.insertOne(newProduct);
+    res.send(result)
+})
+
+app.delete('/service/:id',async(req,res) =>{
+    const id = req.params.id;
+    const query = {_id: new  ObjectId(id)}
+    const result = await serviceCollection.deleteOne(query)
+    res.send(result);
+})
+app.get('/service/:id',async(req,res) =>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await serviceCollection.findOne(query)
+    res.send(result);
+})
+
+app.put('/service/:id',async(req,res)=>{
+    const id = req.params.id;
+    const updatedProduct = req.body;
+    const filter = {_id: new ObjectId(id) };
+    const options = { upsert: true };
+    const userUpdate = {
+      $set: {
+        serviceName:updatedProduct.serviceName,
+             name:updatedProduct.name,
+            image:updatedProduct.image,
+            email:updatedProduct.email,           
+            area:updatedProduct.area,
+
+           price:updatedProduct.price,
+    shortDesc:updatedProduct.shortDesc
+      },
+    }
+    const result = await serviceCollection.updateOne(filter, userUpdate, options);
+    res.send(result)
+
+   })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
 
-app.use(cors())
-app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
